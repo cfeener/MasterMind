9ed9ba7dffe6b2776a0ab99ec3a052a745a7b604
Christopher Feener <christopher.feener@outlook.com>
2019-03-01 23:59:29 -0800

    Computer code-breaker side generates random guesses! Initiating MiniMax method from Wiki.

--- breaker.c
+++ breaker.c
@@ -4,31 +4,33 @@
 
 #include "code.h"
 
-/*
-        int i, n = b->num_colors;
-        b->answer = (int *)calloc(n, sizeof(int));
-        srand(time(0));
-        if (!b->dup_allowed) {  //Based on https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
-                for (i = 0; i < n; i++) {       //Initial set of numbers
-                        b->answer[i] = i + 1;
-                }
-                for (i = n - 1; i > 0; i--)  {  //Shuffle numbers using random indices
-                        int j = rand() % (i + 1);
-                        swap(&b->answer[i], &b->answer[j]);
-                }
-        }
-*/
-
 int * makeGuess(Board * b, int i) {	//Random at first
 	int n = b->row_size, mod = b->num_colors;
-	int j;
-	srand(time(0) + 1);
-	int * a = calloc(n, sizeof(int));
-	for (j = 0; j < n; j++) {
-		a[j] = rand() % mod + 1;
+	int * a = (int *)calloc(n, sizeof(int));
+	if (i == 0) {	//First guess.
+		int j;
+		for (j = 0; j < n; j++) 
+			a[j] = j;
+	} else {	//Using Fisher-Yates shuffle. 
+		int j;
+		for (j = 0; j < n; j++)
+			a[j] = j + 1;
+		for (j = n - 1; j > 0; j--) {
+			int k = rand() % (j + 1);
+			swap(&a[j], &a[k]);
+		}
 	}
 	return a;
 }
+
+/* Note: No idea how to go forward other than making giant array of possible answers,
+	then going through them every time a guess is made.
+ *
+ */
+/*void analyzeResponse(Board * b) {	//TODO: Rule out possible guesses out of 1296?!
+	printf("Coming soon\n");
+	return;
+}*/
 
 int main(void) {
         int num_colors = 6;
@@ -36,27 +38,32 @@ int main(void) {
         int row_size = 4;
         int dup_allowed = false;
 
+	srand(time(0));	//Allows for different values for each iteration.
+
         printf("Welcome to MasterMind-> Code Breaker!\n");
-        num_colors = input("Colors (leave blank for default 6): ", num_colors);
-        num_rows = input("Number of guesses (leave blank for default 8): ", num_rows);
-        row_size = input("Length of code (leave blank for default 4): ", row_size);
-        dup_allowed = b_input("NO duplicates in answer (leave blank for default): ");
-        printf("Colors: %i, Guesses: %i, Length: %i, ", num_colors, num_rows, row_size);
-        if (dup_allowed)
-                printf("Duplicates in answer\n");
-        else printf("Duplicates NOT in answer\n");
+	/* Base algorithm should work on default settings as shown on Wiki: 
+		https://en.wikipedia.org/wiki/Mastermind_(board_game)#Algorithms*/
 
 	//Computer makes its own answer in set up.
         Board * B = createBoard(num_rows, row_size, num_colors, dup_allowed);
 
 	//Computer finds that answer
 	int i = 0;
+	bool is_won = false;
 	while (i < num_rows) {
 		int * a = makeGuess(B, i);
-		checkGuess(B, a, i);
+		if (checkGuess(B, a, i)) {
+			is_won = true;
+			break;
+		}
+		//analyzeResponse(B);
 		printBoard(B);
 		i++;
 	}
+
+	if (is_won)
+		printf("I won!\n");
+	else printf("I lost.\n");
 
 	freeBird(B);
 
